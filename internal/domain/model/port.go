548614beae981f23e9ae43f4dad8b3f7513aa62a
Diego Siqueira <mail@diegosiqueira.xyz>
2023-05-01 03:25:14 -0300

    refactor: remove json tags from domain model

--- internal/domain/model/port.go
+++ internal/domain/model/port.go
@@ -6,16 +6,16 @@ import "context"
 
 type (
 	Port struct {
-		Name        string    `json:"name"`
-		City        string    `json:"city"`
-		Country     string    `json:"country"`
-		Alias       []string  `json:"alias"`
-		Regions     []string  `json:"regions"`
-		Coordinates []float64 `json:"coordinates"`
-		Province    string    `json:"province"`
-		Timezone    string    `json:"timezone"`
-		Unlocs      []string  `json:"unlocs"`
-		Code        string    `json:"code"`
+		Name        string
+		City        string
+		Country     string
+		Alias       []string
+		Regions     []string
+		Coordinates []float64
+		Province    string
+		Timezone    string
+		Unlocs      []string
+		Code        string
 	}
 
 	Storage interface {
